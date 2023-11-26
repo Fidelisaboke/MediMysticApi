ae990a31d57dc61fc8bde4e7c951c621ba9592be
fidelisaboke <fidelisaboke@gmail.com>
2023-11-26 21:03:28 +0300

    Remove whitespace in check subscription middleware alias

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'check.  subscription'=> \App\Http\Middleware\CheckSubscription::class,
+        'check.subscription'=> \App\Http\Middleware\CheckSubscription::class,
     ];
 }
